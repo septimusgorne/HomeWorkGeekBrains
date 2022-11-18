21e8221d4d2108b7112cd3ad939d3174345e61f7
septimusgorne <97679516+septimusgorne@users.noreply.github.com>
2022-11-18 20:14:55 +0500

    commit Fifth Homework

--- 2 четверть/Python_Intro/Fifth_Homework/2_Sweet_Game.py
+++ 2 четверть/Python_Intro/Fifth_Homework/2_Sweet_Game.py
@@ -6,27 +6,6 @@
 
 # b) Подумайте как наделить бота ""интеллектом""
 
-
-
-# count_of_sticks = int(input('введите количество палочек для игры: '))
-# gamer_1, gamer_2 = input('введите имя 1 игрока: '), input('введите имя 2 игрока: ')
-# current_gamer = gamer_1
-# while count_of_sticks > 0:
-#     print('количество оставшихся палочек: {}'.format(count_of_sticks))
-#     while True:
-#         number_to_delete = int(input('ход игрока {} (1 - 3): '.format(current_gamer)))
-#         if number_to_delete >= 1 and number_to_delete <= 3:
-#             break
-#     count_of_sticks -= number_to_delete
-#     current_gamer = gamer_2 if current_gamer == gamer_1 else gamer_1
-
-# print('Победил {}'.format(current_gamer))
-
-# box_sweet = 2021
-# user_1, user_2  = "Первый игрок", "Второй игрок"
-# current_user = user_1
-# rand = random.randint(5, 23)
-
 import random
 
 def inputs():
