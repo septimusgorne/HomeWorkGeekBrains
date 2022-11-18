b69a00362bf1f3360c45bf4f441a1eaf644f3511
septimusgorne <97679516+septimusgorne@users.noreply.github.com>
2022-11-18 20:14:26 +0500

    commit Fifth Homework

--- 2 четверть/Python_Intro/Fifth_Homework/3_Game_Null.py
+++ 2 четверть/Python_Intro/Fifth_Homework/3_Game_Null.py
@@ -1,20 +1,9 @@
 # 3. Создайте программу для игры в ""Крестики-нолики"".
 
-print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
-
-board = list(range(1,30))
-
-# def draw_board(board):
-#    print("-" * 13)
-#    for i in range(3):
-#       print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-#       print("-" * 13)
-
-# draw_board(board)
-
 from colorama import init, Fore, Back, Style 
 
-# Initializes Colorama # Для интереса решил попробовать библиотеку
+board = list(range(1,30))
+# Initializes Colorama # Для интереса решил попробовать библиотеку с цветами
 init(autoreset=True) 
 def draw_board(board):
    print((Style.BRIGHT + Back.GREEN + Fore.RED + "-" )* 13)
@@ -23,54 +12,52 @@ def draw_board(board):
         board[0+i*3], Style.BRIGHT + Back.GREEN + Fore.RED +  "|", 
         board[1+i*3],Style.BRIGHT + Back.GREEN + Fore.RED + "|", board[2+i*3],Style.BRIGHT + Back.GREEN + Fore.RED + "|")
         print((Style.BRIGHT + Back.GREEN + Fore.RED + "-" )* 13)
-#draw_board(board)# Проверка
+#draw_board(board)# Проверка доски
 
 
-def take_input(player_token):
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
+def inputs(item):
+   Tryes = False
+   while not Tryes:
+      choice = input("Выберите ячейку " + item+"? ")
       try:
-         player_answer = int(player_answer)
+         choice = int(choice)
       except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
+         print("Введите номер ячейки")
          continue
-      if player_answer >= 1 and player_answer <= 9:
-         if(str(board[player_answer-1]) not in "XO"):
-            board[player_answer-1] = player_token
-            valid = True
+      if choice >= 1 and choice <= 9:
+         if(str(board[choice-1]) not in "XO"):
+            board[choice-1] = item
+            Tryes = True
          else:
-            print("Эта клетка уже занята!")
+            print("Выберите незанятую клетку")
       else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+        print("Введите номер ячейки от 1 до 9.")
 
-def check_win(board):
-   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
+def check_matrix(board):
+   board_matrix = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for vart in board_matrix:
+       if board[vart[0]] == board[vart[1]] == board[vart[2]]:
+          return board[vart[0]]
    return False
 
 def main(board):
-    counter = 0
+    n = 0
     win = False
     while not win:
         draw_board(board)
-        if counter % 2 == 0:
-           take_input("X")
+        if n % 2 == 0:
+           inputs("X")
         else:
-           take_input("O")
-        counter += 1
-        if counter > 4:
-           tmp = check_win(board)
+           inputs("O")
+        n += 1
+        if n > 4:
+           tmp = check_matrix(board)
            if tmp:
-              print(tmp, "выиграл!")
+              print(tmp, "Win")
               win = True
               break
-        if counter == 9:
-            print("Ничья!")
+        if n == 9:
+            print("Draw")
             break
     draw_board(board)
 main(board)
-
-input("Нажмите Enter для выхода!")
