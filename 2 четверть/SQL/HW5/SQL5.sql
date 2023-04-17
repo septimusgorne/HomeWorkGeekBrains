1f0179858f0b13a10d2c9523a1ad776d32a806a6
SGORNE <97679516+septimusgorne@users.noreply.github.com>
2023-04-17 21:45:38 +0500

    Add files via upload
    sql_hw5

---
+++ 2 четверть/SQL/HW5/SQL5.sql
@@ -0,0 +1,20 @@
+-- 1. Создайте представление, в которое попадает информация о пользователях (имя, фамилия, город и пол), которые не старше 20 лет.
+SELECT firstname, lastname, hometown,  gender
+FROM users, profiles 
+WHERE (YEAR(NOW())-YEAR(birthday)) < 20;
+
+-- 2. Найдите кол-во, отправленных сообщений каждым пользователем и выведите ранжированный список пользователей, указав имя и фамилию пользователя, количество отправленных сообщений и место в рейтинге (первое место у пользователя с максимальным количеством сообщений) . (используйте DENSE_RANK)
+SELECT 
+	DENSE_RANK() OVER (ORDER BY COUNT(from_user_id) DESC) AS 'Место в рейтинге',
+	COUNT(from_user_id) AS 'Количество отправленных сообщений',
+	CONCAT(firstname, ' ', lastname) AS 'Пользователи'
+FROM users u
+JOIN messages m ON u.id = m.from_user_id
+GROUP BY u.id
+;
+
+-- 3. Выберите все сообщения, отсортируйте сообщения по возрастанию даты отправления (created_at) и найдите разницу дат отправления между соседними сообщениями, получившегося списка. (используйте LEAD или LAG)
+SELECT *, 
+LAG(created_at, 1, 0) OVER (PARTITION BY TIMESTAMPDIFF(SECOND, created_at, created_at)) AS lag_created_at, -- смещение на 1 и вместо NULL будет 0
+LEAD(created_at) OVER (PARTITION BY TIMESTAMPDIFF(SECOND, created_at, created_at)) AS lead_created_at
+FROM messages ORDER BY TIMESTAMPDIFF(SECOND, created_at, NOW()) DESC;
